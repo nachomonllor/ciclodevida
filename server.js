a4ac3e1732a584289559f36bfe00d3b6e6e72fe7
nachomonllor <nachomonllorc@gmail.com>
2020-04-05 02:22:45 -0300

    nuevo commit

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/angularapp'));
+app.use(express.static(__dirname + '/ciclodevida2'));
 
 app.listen(process.env.PORT || 8080);
 
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/angularapp/index.html'));
+    res.sendFile(path.join(__dirname + '/ciclodevida2/index.html'));
 });
 
 console.log('Finalizo!!');
